b1131a6f28190ffea5f4920f04009cb8d73d8639
Matt Duff <matt.mpdee@gmail.com>
2026-07-26 20:04:59 +0100

    chore(finalise): repo finalisation

--- src/lib/app-version.ts
+++ src/lib/app-version.ts
@@ -8,7 +8,7 @@ export interface AppVersionInfo {
 }
 
 export const APP_VERSION_INFO: AppVersionInfo = {
-  version: '2.1.29',
+  version: '2.1.30',
 };
 
 export const APP_VERSION = APP_VERSION_INFO.version;
